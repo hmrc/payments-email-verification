7dec54335135ab3ef8bd14556c082b660e508294
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2025-02-04 16:43:19 +0000

    fix

--- cor/shared/src/main/scala/paymentsEmailVerification/models/DerivedJson.scala
+++ cor/shared/src/main/scala/paymentsEmailVerification/models/DerivedJson.scala
@@ -19,21 +19,37 @@ package paymentsEmailVerification.models
 import play.api.libs.json.*
 import zio.json.*
 
-
 object DerivedJson {
 
   def oformat[A](encoder: JsonEncoder[A], decoder: JsonDecoder[A]): OFormat[A] = {
     new OFormat[A] {
       override def writes(o: A): JsObject =
         Json.parse(encoder.encodeJson(o).toString) match
-          case j:JsObject => j
-          case s:JsString => JsObject(Map(s.value -> JsObject.empty))
+          case j: JsObject => j
+          // if sealed trait is extended only be case objects then zio will write JsString's instead of JsObjects -
+          // make sure we write an empty js object in those cases
+          case s: JsString => JsObject(Map(s.value -> JsObject.empty))
           case e => throw new Exception(s"Unexpected json type: ${e.getClass.getSimpleName}")
 
-      override def reads(json: JsValue): JsResult[A] =
-        decoder
-          .decodeJson(json.toString)
-          .fold[JsResult[A]](e => JsError(e), JsSuccess(_))
+      override def reads(json: JsValue): JsResult[A] = {
+        def doReads(j: JsValue): JsResult[A] =
+          decoder
+            .decodeJson(j.toString)
+            .fold[JsResult[A]](JsError(_), JsSuccess(_))
+
+        doReads(json).recoverWith { e =>
+          // we may have written an empty JsObject when zio would have given a JsString before (see comment in writes)
+          json match {
+            case JsObject(map) =>
+              map.toList match {
+                case (s, j: JsObject) :: Nil if j.value.isEmpty =>
+                  doReads(JsString(s))
+                case _ => e
+              }
+            case _ => e
+          }
+        }
+      }
 
     }
   }
